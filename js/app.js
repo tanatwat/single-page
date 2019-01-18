5a90e13349c3f336a65ff4dbbd0ac36f791fae64
Tanatwat Wattanasakul <scomusicrusted@gmail.com>
2019-01-18 16:47:49 +0700

    clean file structure

--- js/app.js
+++ js/app.js
@@ -1,28 +1,57 @@
 import Vue from 'vue';
-import { mixin as clickaway } from 'vue-clickaway';
+import {
+  mixin as clickaway
+} from 'vue-clickaway';
+import smoothReflow from 'vue-smooth-reflow';
+
+import AOS from 'aos';
 
 window.document.addEventListener("DOMContentLoaded", function() {
   const app = new Vue({
     el: '#app',
-    mixins: [ clickaway ],
+    mixins: [clickaway, smoothReflow],
     data() {
       return {
-        dropdown: null,
-        menuToggle: false
+        dropdown: false,
+        dropdownMobile: false,
+        menuToggle: false,
+        banners: ['#', '#']
       };
     },
     methods: {
       toggleDropdown(id) {
+        // Vanilla version
         if (id) {
           if (id === this.dropdown) {
             this.dropdown = null
           } else {
             this.dropdown = id
           }
-        } else {
+        } else {',
+      })
+    },
           this.dropdown = null
         }
+      },
+      closeDropdown() {
+        this.dropdown = false
       }
+    },
+    mounted() {
+      this.$smoothReflow({
+        el: '.mobile-dropdown',
+      })
+    },
+    created() {
+      AOS.init({
+        offset: 120, // offset (in px) from the original trigger point
+        delay: 150, // values from 0 to 3000, with step 50ms
+        duration: 200, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        once: true, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+      });
     }
   })
 });
